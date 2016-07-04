2b65d53b087558da81f7b1ff9b3366245a982293
Kamel Derouiche <kamel.derouiche@gmail.com>
2016-07-04 12:47:55 +0100

    new func

--- main.go
+++ main.go
@@ -8,16 +8,23 @@ import (
 )
 
 var (
-	_appName  string = "arxquery"
-	_appUsage string = "experimental arxivprog!"
+	_appName    string = "arxivqry"
+	_appUsage   string = "Get publication metadata from arxiv.org"
+	_appVersion string = "0.0.1pre1"
+	_appAuthor  string = "K.I.A.Derouiche"
 )
 
 func main() {
 
+	newApp().Run(os.Args)
+}
+
+func newApp() *cli.App {
 	app := cli.NewApp()
 	app.Name = _appName
 	app.Usage = _appUsage
-
+	app.Version = _appVersion
+	app.Author = _appAuthor
 	app.Commands = commands.Commands
-	app.Run(os.Args)
+	return app
 }
