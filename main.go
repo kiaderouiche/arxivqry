975af83e6e56f0436c36fbe31be62b769bb2c1fa
Kamel Derouiche <kamel.derouiche@gmail.com>
2016-07-04 12:50:45 +0100

    add email dev

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ var (
 	_appUsage   string = "Get publication metadata from arxiv.org"
 	_appVersion string = "0.0.1pre1"
 	_appAuthor  string = "K.I.A.Derouiche"
+	_appEmail   string = "kamel.derouiche@gmail.com"
 )
 
 func main() {
@@ -25,6 +26,7 @@ func newApp() *cli.App {
 	app.Usage = _appUsage
 	app.Version = _appVersion
 	app.Author = _appAuthor
+	app.Email = _appEmail
 	app.Commands = commands.Commands
 	return app
 }
