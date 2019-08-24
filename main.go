5e21f9858ec6b00fe13e168a7c14d5540d6b7436
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2019-08-24 17:07:16 +0100

    module support

--- main.go
+++ main.go
@@ -7,26 +7,21 @@ import (
 	"github.com/urfave/cli"
 )
 
-var (
-	_appName    string = "arxivqry"
-	_appUsage   string = "Get publication metadata from arxiv.org"
-	_appVersion string = "0.0.1pre2"
-	_appAuthor  string = "K.I.A.Derouiche"
-	_appEmail   string = "kamel.derouiche@gmail.com"
-)
-
-func main() {
-
-	newApp().Run(os.Args)
+func info() {
+	app.Name = "arxivqry"
+	app.Usage = "Get publication metadata from arxiv.org"
+	app.Version = "0.0.1pre2"
+	app.Author = "K.I.A.Derouiche"
+	app.Email = "kamel.derouiche@gmail.com"
 }
 
 func newApp() *cli.App {
 	app := cli.NewApp()
-	app.Name = _appName
-	app.Usage = _appUsage
-	app.Version = _appVersion
-	app.Author = _appAuthor
-	app.Email = _appEmail
 	app.Commands = commands.Commands
 	return app
+}
+
+func main() {
+
+	newApp().Run(os.Args)
 }
