ac0710f34a9b1bb35ad0aea0f4d3e8c3f606c040
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2019-08-26 21:51:17 +0100

    new code

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/kiaderouiche/arxivqry
 
 go 1.12
+
+require (
+	github.com/urfave/cli v1.21.0
+)
--- main.go
+++ main.go
@@ -3,21 +3,13 @@ package main
 import (
 	"os"
 
-	"github.com/kiaderouiche/arxivqry/commands"
 	"github.com/urfave/cli"
 )
 
-func info() {
-	app.Name = "arxivqry"
-	app.Usage = "Get publication metadata from arxiv.org"
-	app.Version = "0.0.1pre2"
-	app.Author = "K.I.A.Derouiche"
-	app.Email = "kamel.derouiche@gmail.com"
-}
-
 func newApp() *cli.App {
+
 	app := cli.NewApp()
-	app.Commands = commands.Commands
+
 	return app
 }
 
